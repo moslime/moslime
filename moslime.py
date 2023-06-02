79fc86aa4630393f53da25726c4632ae09fe009b
lmore377 <24685455+lmore377@users.noreply.github.com>
2023-06-02 04:05:37 -0600

    Update moslime.py
    add a ton of comments and a few lil fixes

--- moslime.py
+++ moslime.py
@@ -18,12 +18,13 @@ TPS = CONFIG['tps']  # SlimeVR packet frequency. Keep below 300 (above 300 has w
 
 
 def build_handshake():
+    fw_string = "MoSlime"
     buffer = b'\x00\x00\x00\x03'  # packet 3 header
-    buffer += struct.pack('>Q', PACKET_COUNTER)
-    buffer += struct.pack('>LLLLLLL', 0, 0, 0, 0, 0, 0, 0)
-    buffer += struct.pack('B', len("MoSlime"))
-    buffer += struct.pack(str(len("MoSlime")) + 's', "MoSlime".encode('UTF-8'))
-    buffer += struct.pack('6s', '111111'.encode('UTF-8'))
+    buffer += struct.pack('>Q', PACKET_COUNTER) # packet counter
+    buffer += struct.pack('>LLLLLLL', 0, 0, 0, 0, 0, 0, 0) # padding 
+    buffer += struct.pack('B', len(fw_string)) #length of fw string
+    buffer += struct.pack(str(len(fw_string)) + 's', fw_string.encode('UTF-8')) #fw string
+    buffer += struct.pack('6s', '111111'.encode('UTF-8')) #MAC address (just using a placeholder of 31:31:31:31:31:31 for now)
     buffer += struct.pack('B', 255)
     return buffer
 
@@ -31,10 +32,10 @@ def build_handshake():
 def add_imu(trackerID):
     global PACKET_COUNTER
     buffer = b'\x00\x00\x00\x0f'  # packet 15 header
-    buffer += struct.pack('>Q', PACKET_COUNTER)
-    buffer += struct.pack('B', trackerID)
-    buffer += struct.pack('B', 3)
-    buffer += struct.pack('B', 5)
+    buffer += struct.pack('>Q', PACKET_COUNTER) #packet counter
+    buffer += struct.pack('B', trackerID) #tracker id (shown as IMU Tracker #x in SlimeVR)
+    buffer += struct.pack('B', 3) #sensor status (placeholder, seems to not be used by SlimeVR currently)
+    buffer += struct.pack('B', 5) #sensor type (placeholder, seems to not be used by SlimeVR currently)
     buffer += struct.pack('B', 255)
     sock.sendto(buffer, (SLIME_IP, SLIME_PORT))
     print("Add IMU: " + str(trackerID))
@@ -42,21 +43,21 @@ def add_imu(trackerID):
 
 
 def build_rotation_packet(qw: float, qx: float, qy: float, qz: float, tracker_id: int):
-    # quant: Array containing the quanterion / trackerID: Tracker ID
+    # qw,qx,qy,qz: parts of a quaternion / trackerID: Tracker ID
     buffer = b'\x00\x00\x00\x11'  # packet 17 header
-    buffer += struct.pack('>Q', PACKET_COUNTER)
-    buffer += struct.pack('B', tracker_id)
-    buffer += struct.pack('B', 1)
-    buffer += struct.pack('>ffff', -qx, qz, qy, qw)  # expect x,z,y,w
-    buffer += struct.pack('B', 0)
+    buffer += struct.pack('>Q', PACKET_COUNTER) #packet counter
+    buffer += struct.pack('B', tracker_id) #tracker id (shown as IMU Tracker #x in SlimeVR)
+    buffer += struct.pack('B', 1) # data type (use is unknown)
+    buffer += struct.pack('>ffff', -qx, qz, qy, qw)  # quaternion as x,z,y,w
+    buffer += struct.pack('B', 0) # calibration info (seems to not be used by SlimeVR currently)
     return buffer
 
 
 def build_accel_packet(ax, ay, az, trackerID):
     buffer = b'\x00\x00\x00\x04'  # packet 4 header
-    buffer += struct.pack('>Q', PACKET_COUNTER)
-    buffer += struct.pack('>fff', ax, ay, az)
-    buffer += struct.pack('B', trackerID)
+    buffer += struct.pack('>Q', PACKET_COUNTER) # packet counter
+    buffer += struct.pack('>fff', ax, ay, az) #acceleration as x y z
+    buffer += struct.pack('B', trackerID)#tracker id (shown as IMU Tracker #x in SlimeVR)
     return buffer
 
 
@@ -67,23 +68,15 @@ def sendCommand(tId, cmd):  # id: Tracker ID / cmd: Command to send
         print("Invalid command")
 
 
-interp = interp1d([-8192, 8192], [-1, 1])
-
-
+interp = interp1d([-8192, 8192], [-1, 1]) # mocopi sends quat as a signed int from -8192to8192 but quats are -1to1. this scales down the mocopi data
 def hexToQuat(bytes):
     return interp(int.from_bytes(bytes, byteorder='little', signed=True))
-
-
 def hexToFloat(bytes):
     return np.frombuffer(bytes, dtype=np.float16)
-
-
-def waitForNotif(num):
+def waitForNotif(num): #starts the notification listener for a given trackerID. should be ran on its own thread
     while True:
         globals()['t' + str(num)].waitForNotifications(0)
-
-
-def multiply(w1, x1, y1, z1, w2, x2, y2, z2):
+def multiply(w1, x1, y1, z1, w2, x2, y2, z2): #multiply a quat by another
     w = w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2
     x = w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2
     y = w1 * y2 + y1 * w2 + z1 * x2 - x1 * z2
@@ -91,12 +84,12 @@ def multiply(w1, x1, y1, z1, w2, x2, y2, z2):
     return w, x, y, z
 
 
-PACKET_COUNTER = 0
-CMD_UUID = '0000ff00-0000-1000-8000-00805f9b34fb'
+PACKET_COUNTER = 0 # global packet counter. MUST be incremented every time a packet is sent or slime gets mad
+CMD_UUID = '0000ff00-0000-1000-8000-00805f9b34fb' # BLE UUID to send commands to
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 ZERO_QUAT = [1, 0, 0, 0]
 ALL_CONNECTED = False
-MocopiPacket = namedtuple('MocopiPacket', 'sensor_id, qw, qx, qy, qz, ax, ay, az')
+MocopiPacket = namedtuple('MocopiPacket', 'sensor_id, qw, qx, qy, qz, ax, ay, az') # container that holds the data of a given trackerID
 for i in range(len(TRACKER_ADDRESSES)):
     globals()['sensor' + str(i) + 'data'] = MocopiPacket(0, 0, 0, 0, 0, 0, 0, 0)  # Create tracker data containers
 
@@ -134,8 +127,7 @@ def connectTracker(mac_addr, tId):
             continue
         break
 
-
-def correct(aX, aY, aZ):
+def correct(aX, aY, aZ): #will be used to correct accel data from tracker
     SEN4G = 8192.0
     ASC4G = ((32768.0 / SEN4G) / 32768.0) * 9.80665
     aX2 = aX * ASC4G
@@ -143,8 +135,7 @@ def correct(aX, aY, aZ):
     aZ2 = aZ * ASC4G
     return aX2, aY2, aZ2
 
-
-class NotificationHandler(btle.DefaultDelegate):
+class NotificationHandler(btle.DefaultDelegate): #takes in tracker data, applies any needed corrections and saves it to the container for a given trackerID
     trakID = 0
     ignorePackets = -10
     # When Mocopi trackers start they sometimes send a few packets that aren't IMU data so we just discard the first
@@ -160,6 +151,7 @@ class NotificationHandler(btle.DefaultDelegate):
         global ZERO_QUAT, ALL_CONNECTED
         if ALL_CONNECTED:  # Don't collect data until all trackers successfully connect
             try:
+                # take in hex data and chop it up into usable stuff
                 pw = hexToQuat(data[8:10])
                 px = hexToQuat(data[10:12])
                 py = hexToQuat(data[12:14])
@@ -174,16 +166,17 @@ class NotificationHandler(btle.DefaultDelegate):
                     return
                 elif self.ignorePackets < 0:
                     self.ignorePackets += 1
-                qwc, qxc, qyc, qzc = multiply(pw, px, py, pz, *self.offset)
-                az, ay, az = correct(ax, ay, az)
+                qwc, qxc, qyc, qzc = multiply(pw, px, py, pz, *self.offset) #apply quat offset/correctiom
+                az, ay, az = correct(ax, ay, az) #apply accel offset
                 globals()['sensor' + str(self.trakID) + 'data'] = MocopiPacket(self.trakID, qwc, qxc, qyc, qzc, ax, ay,
-                                                                               az)
+                                                                               az) #store tracker data in its container
             except Exception as e:
                 print("class exception: " + str(e) + " trackerid: " + str(self.trakID))
 
-
+print("Connecting will start in 10s. Turn on all your trackers, place them on a table and don't touch them until you see Safe to start tracking."
+time.sleep(10)
 print("Connecting to " + str(len(TRACKER_ADDRESSES)) + " trackers.")
-# Connect all trackers then send start command
+# Connect all trackers then send start command to each one
 for i in range(len(TRACKER_ADDRESSES)):
     connectTracker(TRACKER_ADDRESSES[i], i)
 for i in range(len(TRACKER_ADDRESSES)):
@@ -191,8 +184,8 @@ for i in range(len(TRACKER_ADDRESSES)):
 time.sleep(3)  # Give trackers a few seconds to stabilize
 # Send the initial handshake to SlimeVR
 handshake = build_handshake()
-PACKET_COUNTER += 1
 sock.sendto(handshake, (SLIME_IP, SLIME_PORT))
+PACKET_COUNTER += 1
 print("Handshake")
 time.sleep(.1)
 
