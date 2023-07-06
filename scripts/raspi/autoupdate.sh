2df5ae1e2f861ba25d2b25d07c60cb0bd9f57f02
lmore377 <24685455+lmore377@users.noreply.github.com>
2023-07-05 20:01:34 -0600

    Update autoupdate.sh

--- scripts/raspi/autoupdate.sh
+++ scripts/raspi/autoupdate.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-# not used right now. will be used if a new package or something needs to be installed.
-echo
+# Changelog (num = number at end of noautoupd):
+# 1. raspi folder was moved, launcher.sh can't update itself
+
+FILE=/home/moslime/noautoupd1
+if test -f "$FILE"; then
+    echo "$FILE exists. Don't run autoupdate.sh"
+else
+    cp /home/moslime/moslime/scripts/raspi/* /home/moslime
+    touch /home/moslime/noautoupd1
+fi
